7f74d8ea59b02686f66b28bd22ffe429a18ca11e
nathan2lam <133164840+nathan2lam@users.noreply.github.com>
2024-04-05 17:04:15 -0500

    Update Display.jsx

--- src/Page/Display.jsx
+++ src/Page/Display.jsx
@@ -31,6 +31,7 @@ export default function Display({ weatherData }) {
         case "Smoke":
             condition = "smoky";
             src = "smoky";
+            break;
         default:
             break;
     }
